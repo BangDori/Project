f3411eb5f0c1751da3fab9088f98b8eea4a501d3
BangDori <qudwns8865@naver.com>
2022-09-10 00:38:03 +0900

    create login

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+NAME = 'APP'
 urlpatterns = [
     path('', views.index, name='app'),
+    path('login/', views.login, name='login'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+def login(request):
+    return render(request, 'app/login.html')
