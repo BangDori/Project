ec9cd3a539aaffc64eb2c9d81268cb277da2d76a
Toygoon <toygoon@gmail.com>
2022-09-22 13:47:26 +0900

    Add: [Login 구현] Admin 페이지에 뜨는 항목 수정

--- bangdori/admin.py
+++ bangdori/admin.py
@@ -6,7 +6,7 @@ from bangdori.models import CustomerUser
 # Register your models here.
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('userid', 'email', 'passwd', 'birthday', 'phone')
+    list_display = ('username', 'email', 'password', 'birthday', 'phone')
 
 
 admin.site.register(CustomerUser, UserAdmin)
