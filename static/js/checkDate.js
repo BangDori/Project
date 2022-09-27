bc22a7aa847522593b47d6d2af7ad490915e90eb
BangDori <qudwns8865@naver.com>
2022-09-27 16:46:18 +0900

    Edit: 게시판 하단부 검색창 디자인 변경

--- static/js/checkDate.js
+++ static/js/checkDate.js
@@ -1,28 +1,15 @@
 const dates = document.querySelectorAll(".js-date");
 
-function checkNow() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = now.getMonth();
-    const day = now.getDate();
-
-    console.log(now);
-    console.log(year, month+1, day);
-
-    board_date = checkBoardDate();
-}
-
 function checkBoardDate() {
     const now = new Date();
 
     dates.forEach(date => {
         const tmp = date.innerHTML.split(/[년|월|일| ]/);
+
         const board_time = tmp.filter(i => {
             return i !== '';
         }) 
 
-        console.log(board_time);
-        // console.log(a[3])
         if(now.getFullYear() > parseInt(board_time[0])) {
             date.innerHTML = `${now.getFullYear() - parseInt(board_time[0])}년 전`;
         } else if(now.getMonth() > parseInt(board_time[1])) {
@@ -36,19 +23,14 @@ function checkBoardDate() {
                 const t = board_time[3].split(':');
                 t[0] = `${parseInt(t[0]) + 12}`;
                 board_time[3] = t.join(":");
-                console.log(board_time[3]);
                 date.innerHTML = board_time[3];
-                // const t = a[4].split(':');
-                // console.log(t);
-                // date.innerHTML = 
             }
         }
     })
-
 }
 
 function init() {
-    checkNow();
+    checkBoardDate();
 }
 
 init();
