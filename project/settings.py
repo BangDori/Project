d92088e03aa8e4e77b542daf8b0d2358681583a5
BangDori <qudwns8865@naver.com>
2022-09-27 13:47:03 +0900

    Edit: 언어, 시간 설정

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+LANGUAGE_CODE = 'ko-kr'
+TIME_ZONE = 'Asia/Seoul'
+TSE_TZ = False
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
