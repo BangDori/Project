139d102a533e1cfdf1bf12397e5a2362fbe638dd
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2022-09-27 14:46:47 +0900

    Update checkDate.js

--- static/js/checkDate.js
+++ static/js/checkDate.js
@@ -1,17 +1,5 @@
 const dates = document.querySelectorAll(".js-date");
 
-function checkNow() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = now.getMonth();
-    const day = now.getDate();
-
-    console.log(now);
-    console.log(year, month+1, day);
-
-    board_date = checkBoardDate();
-}
-
 function checkBoardDate() {
     const now = new Date();
 
@@ -21,8 +9,6 @@ function checkBoardDate() {
             return i !== '';
         }) 
 
-        console.log(board_time);
-        // console.log(a[3])
         if(now.getFullYear() > parseInt(board_time[0])) {
             date.innerHTML = `${now.getFullYear() - parseInt(board_time[0])}년 전`;
         } else if(now.getMonth() > parseInt(board_time[1])) {
@@ -36,19 +22,14 @@ function checkBoardDate() {
                 const t = board_time[3].split(':');
                 t[0] = `${parseInt(t[0]) + 12}`;
                 board_time[3] = t.join(":");
-                console.log(board_time[3]);
                 date.innerHTML = board_time[3];
-                // const t = a[4].split(':');
-                // console.log(t);
-                // date.innerHTML = 
             }
         }
     })
-
 }
 
 function init() {
-    checkNow();
+    checkBoardDate();
 }
 
 init();
