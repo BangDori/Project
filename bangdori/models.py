a848a933b38d9c9f79e69d2da0159c441e362c81
Toygoon <toygoon@gmail.com>
2022-09-28 13:05:26 +0900

    Add: 사용자 : 닉네임, 비활성화 날짜, 사업자 등록번호 추가 / 게시글 : __dict__ 추가

--- bangdori/models.py
+++ bangdori/models.py
@@ -15,24 +15,31 @@ class CustomerUser(AbstractUser):
     Parameters
     ----------
     email : CharField
-        이메일 저장
-    birthday : CharField
-        수정할 것
+        이메일
+    birthday : DateField
+        생일
     phone : CharField
-        전화번호 영역
+        전화번호
+    nickname : CharField
+        닉네임
+    blocked_at : DateField
+        (예정 : is_active 필드와 함께 작동) 사용자가 비활성화된 날짜를 알려줌
+    corp_num : CharField
+        사업자 등록번호
     """
 
-    ''' # 부모 클래스에서 제공하는 부분은 비활성화
-    # userid = models.CharField(
-    #     max_length=50, db_column='userid', verbose_name='userid', primary_key=True)
-    # passwd = models.CharField(
-    #     max_length=50, db_column='passwd', verbose_name='passwd') '''
     email = models.CharField(
         max_length=30, db_column='email', verbose_name='email', blank=True)
     birthday = models.DateField(
         default=datetime.MINYEAR, db_column='birth', verbose_name='birth')
     phone = models.CharField(
         max_length=30, db_column='phone', verbose_name='phone')
+    nickname = models.CharField(
+        max_length=10, db_column='nickname', verbose_name='nickname')
+    blocked_at = models.DateTimeField(
+        default=datetime.MINYEAR, db_column='blocked_at', verbose_name='blocked_at')
+    corp_num = models.CharField(
+        max_length=10, blank=True, db_column='corp_num', verbose_name='corp_num')
 
     def __str__(self):
         return self.username
@@ -81,6 +88,12 @@ class Article(models.Model):
     def __str__(self):
         # __str__ 오버라이드로 제목만 표시
         return self.title
+
+    def __dict__(self):
+        # __dict__ 오버라이드로 dict 전달
+        return {'id': self.id, 'title': self.title, 'writer': self.writer.nickname,
+                'content': self.content, 'date': self.date, 'views': self.views,
+                'upvote': self.upvote}
 
     class Meta:
         # Meta 클래스 오버라이드로 상세 내용 지정 (Form을 위함)
