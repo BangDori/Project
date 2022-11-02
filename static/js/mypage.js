23f9fbeabe3472908d0cabecdd4797c323767060
HmDol <112683455+HmDol@users.noreply.github.com>
2022-11-02 14:57:37 +0900

    Add: 마이페이지 화면 디자인 1차 완성

---
+++ static/js/mypage.js
@@ -0,0 +1,11 @@
+//function test(){
+//                    var conlist = document.querySelectorAll('.content')
+//                    var i = 0;
+//                    while(i < conlist.length){
+//                        conlist[i].style.display = 'none';
+//                        i = i+1;
+//                    }
+//                    document.querySelector('#myinfo-box').style.display = 'block';
+//
+//        }
+
