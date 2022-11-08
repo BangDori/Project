2aa3dbc02f8334aca4545a5f69dcb4aa19347ae0
HmDol <112683455+HmDol@users.noreply.github.com>
2022-11-08 11:48:13 +0900

    Add: 마이페이지 구현

--- bangdori/admin.py
+++ bangdori/admin.py
@@ -7,15 +7,10 @@ from .models import Authentication
 
 
 class UserAdmin(admin.ModelAdmin):
-<<<<<<< HEAD
+
     list_display = ('username', 'email', 'password', 'birthday', 'phone')
-=======
-<<<<<<< HEAD
-    list_display = ('username', 'email', 'password', 'birthday', 'phone', 'nickname')
-=======
-    list_display = ('username', 'email', 'password', 'birthday', 'phone','nickname')
->>>>>>> 1156ba765b0c3afabe30ee719ac3ef0204e2f9c5
->>>>>>> 2f71611ec921cc5e5e5e76a0ae0d74c53bc3f202
+
+
 
 
 class SMSAdmin(admin.ModelAdmin):
