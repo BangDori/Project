bd76fa8ed3be89e7af07527ebcec11cc26e0f787
Toygoon <toygoon@gmail.com>
2022-11-21 13:39:37 +0900

    Add: Article을 JSON 형식으로 출력하는 API 테스트 중

--- project/settings.py
+++ project/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'bangdori',
     'profileapp',
+    'api',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
