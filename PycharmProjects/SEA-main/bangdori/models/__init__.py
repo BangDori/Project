691a0a7f548e7b8bef7d558e5a8e72dc16ac4935
Toygoon <toygoon@gmail.com>
2022-11-30 18:13:23 +0900

    ?

--- PycharmProjects/SEA-main/bangdori/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Address import *
-from .Article import *
-from .Authentication import *
-from .Comment import *
-from .CustomerUser import *
-from .UpvoteHistory import *
--- PycharmProjects/SEA-main/bangdori/views/DetailView.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import DetailView
-
-from bangdori.models import CustomerUser
-
-
-class DetailView(DetailView):
-    model = CustomerUser
-    context_object_name = 'target_user'
-    template_name = 'myinfo.html'
